da6dc53b2d9d0a2bcb4e111a2afa4a203e4f1c69
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-06 17:06:59 +0530

    Add files via upload

---
+++ AOPxmlConfiguration/src/com/aspect/Loggable.java
@@ -0,0 +1,5 @@
+package com.aspect;
+
+public @interface Loggable {
+
+}
---
+++ SpringAOP/src/com/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package com.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	@Before("execution(	public String getName())")
+	public void LoggingAdvice() {
+		System.out.println("Advicerun.Get method called");
+	}
+
+}
